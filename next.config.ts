f7fbd03273a2f7394f72aa5a312f92aefa38b750
Hassan <hassan@gmail.com>
2025-09-20 22:48:45 -0700

    Main Banner Done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import type { Configuration as WebpackConfig } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config: WebpackConfig) {
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import MainBanner from "@/components/MainBanner";
+
 const page = () => {
   return (
-    <div>
-      Home
+    <div className="mt-10 px-6 md:px-16 lg:px-24 xl:px-32">
+      <MainBanner />
     </div>
   )
 }
